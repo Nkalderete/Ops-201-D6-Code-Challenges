f210b0f46138da28dc5eee99b6392149992e559a
nickolaus <nk.alderete@gmail.com>
2023-02-08 01:26:37 +0000

    code challenge

---
+++ helloworld.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 02 Ops Challenge Solution
+# Author: Nickolaus Alderete
+# Date of latest revision: 07FEB2023
+# Purpose: Print a string to the terminal
+
+
+# Main
+echo "I am working perfectly"
+
+# End
