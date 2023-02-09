673f2a603f5b5044c244845bf851e85f17e8bdcd
Nickolaus Alderete <122177990+Nkalderete@users.noreply.github.com>
2023-02-08 21:58:57 -0600

    Update 201code03.sh

--- 201code03.sh
+++ 201code03.sh
@@ -1,19 +1,4 @@
 
-#!/bin/bash
-
-# Define the log file
-log_file="login_info"
-
-# Check if the log file exists
-if [ -f $log_file ]; then
-  # Print the contents of the log file
-  cat $log_file
-  echo "Found log file"
-else
-  # Print an error message if the log file does not exist
-  echo "Error: This log file does not exist."
-fi
-
 
 
 
