f594a8b368f37e75c64b764e8750d23e110ef30c
nickolaus <nk.alderete@gmail.com>
2023-02-09 01:52:32 +0000

    changes made

---
+++ 201code03.sh
@@ -0,0 +1,41 @@
+
+#!/bin/bash
+
+# Define the log file
+log_file="login_info"
+
+# Check if the log file exists
+if [ -f $log_file ]; then
+  # Print the contents of the log file
+  cat $log_file
+  echo "Found log file"
+else
+  # Print an error message if the log file does not exist
+  echo "Error: This log file does not exist."
+fi
+
+
+
+
+
+
+
+sleep 4
+echo "Here we go!!"
+sleep 2 
+
+echo "201 Code Challenge"
+
+sleep 2
+
+echo "Hello" 
+
+echo "How are you?"
+echo "oh wait......"
+
+sleep 4
+
+echo "good bye" 
+
+
+
