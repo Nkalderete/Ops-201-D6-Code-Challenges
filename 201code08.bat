c68715634862cac62a34100c62c85f46e85a4222
Nickolaus Alderete <122177990+Nkalderete@users.noreply.github.com>
2023-02-15 20:45:50 -0600

    Create 201code08.bat

---
+++ 201code08.bat
@@ -0,0 +1,6 @@
+robocopy "C:\Users\j.thompson\Desktop\My Project Files" "E:\Jorges Work" /E
+
+#C drive location is where files are starting from, E drive is where files get copied to at the end of day
+#he /E option will copy all subdirectories inside starting folder
+#log in to admin to set up task scheduler 
+#enable to run without being logged in
